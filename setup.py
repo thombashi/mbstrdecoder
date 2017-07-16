71308f3e9d371b9db91f0343b9d149beaaf241f3
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-07-16 09:42:50 +0900

    [ci skip] Change email address

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 """
-.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 """
 
 from __future__ import unicode_literals
@@ -32,7 +32,7 @@ setuptools.setup(
     name="mbstrdecoder",
     version="0.2.2",
     author="Tsuyoshi Hombashi",
-    author_email="gogogo.vm@gmail.com",
+    author_email="tsuyoshi.hombashi@gmail.com",
     url="https://github.com/thombashi/mbstrdecoder",
     keywords=["multi-byte character", "unicode", "decoder"],
     license="MIT License",
