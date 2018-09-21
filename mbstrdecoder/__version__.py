43c4a9837141ed1806afa66c10816b00bec72766
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-09-21 23:39:34 +0900

    Bump version

--- mbstrdecoder/__version__.py
+++ mbstrdecoder/__version__.py
@@ -4,8 +4,8 @@ from datetime import datetime
 
 
 __author__ = "Tsuyoshi Hombashi"
-__copyright__ = "Copyright {}, {}".format(datetime.now().year, __author__)
+__copyright__ = "Copyright 2016-{}, {}".format(datetime.now().year, __author__)
 __license__ = "MIT License"
-__version__ = "0.4.2"
+__version__ = "0.5.0"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
