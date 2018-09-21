243177b3da5db6dee893c2010aa59e7800999975
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-09-21 23:30:27 +0900

    Integrate related test functions to a test class

--- test/test_func.py
+++ test/test_func.py
@@ -22,29 +22,29 @@ def test_to_codec_name(value, expected):
     assert to_codec_name(value) == expected
 
 
-@pytest.mark.parametrize(["value", "expected"], [["utf8", "utf_8"], ["utf16", "utf_16"]])
-def test_detect_file_encoding(tmpdir, value, expected):
-    data = dedent(
-        """\
-        # ほげほげ -----
-        print(con.fetch_attr_name_list(table_name))
-        result = con.select(select="*", table_name=table_name)
-        for record in result.fetchall():
-        print(record)
-        """
-    )
-    p_csv = tmpdir.join("test_detect_file_encoding")
+class Test_detect_file_encoding(object):
+    @pytest.mark.parametrize(["value", "expected"], [["utf8", "utf_8"], ["utf16", "utf_16"]])
+    def test_normal(self, tmpdir, value, expected):
+        data = dedent(
+            """\
+            # ほげほげ -----
+            print(con.fetch_attr_name_list(table_name))
+            result = con.select(select="*", table_name=table_name)
+            for record in result.fetchall():
+            print(record)
+            """
+        )
+        p_csv = tmpdir.join("test_detect_file_encoding")
 
-    with open(str(p_csv), "wb") as f:
-        f.write(data.encode(value))
+        with open(str(p_csv), "wb") as f:
+            f.write(data.encode(value))
 
-    assert detect_file_encoding(str(p_csv)) == expected
+        assert detect_file_encoding(str(p_csv)) == expected
 
+    @pytest.mark.skipif("platform.system() == 'Windows'")
+    def test_normal_fifo(self, tmpdir):
+        fifo = tmpdir.join("test_fifo")
 
-@pytest.mark.skipif("platform.system() == 'Windows'")
-def test_detect_file_encoding_fifo(tmpdir):
-    fifo = tmpdir.join("test_fifo")
+        os.mkfifo(str(fifo))
 
-    os.mkfifo(str(fifo))
-
-    assert detect_file_encoding(str(fifo)) is None
+        assert detect_file_encoding(str(fifo)) is None
