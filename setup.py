fb31df6bc010a7ac83ce600abf02558381f67d1e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-10-08 17:17:28 +0900

    Modify extras_require

--- setup.py
+++ setup.py
@@ -71,8 +71,8 @@ setuptools.setup(
     setup_requires=SETUPTOOLS_REQUIRES + PYTEST_RUNNER_REQUIRES,
     tests_require=tests_require,
     extras_require={
-        "build": "wheel",
-        "release": "releasecmd>=0.0.12",
+        "build": ["wheel"],
+        "release": ["releasecmd>=0.0.12"],
         "test": tests_require,
     },
 
