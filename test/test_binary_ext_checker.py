be5d3e379d819d98d5ce1d3266253087c94f1728
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-04-29 17:47:10 +0900

    Modify a test case

--- test/test_binary_ext_checker.py
+++ test/test_binary_ext_checker.py
@@ -9,18 +9,27 @@ from __future__ import unicode_literals
 from os.path import splitext
 
 import pytest
-from faker import Factory
 from mbstrdecoder._binary_ext_checker import binary_exts, is_binary_ext_path
 
 
-fake = Factory.create()
-fake.seed(1)
+try:
+    from faker import Factory  # noqa
 
+    FAKER_INSTALLED = True
+except ImportError:
+    FAKER_INSTALLED = False
 
-@pytest.mark.parametrize(
-    ["value", "expected"],
-    [[".".join([splitext(fake.file_path())[0], binary_ext]), True] for binary_ext in binary_exts]
-    + [["hoge.txt", False], ["foo.md", False]],
-)
-def test_is_binary_ext_path(value, expected):
-    assert is_binary_ext_path(value) == expected
+if FAKER_INSTALLED:
+    fake = Factory.create()
+    fake.seed(1)
+
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [
+            [".".join([splitext(fake.file_path())[0], binary_ext]), True]
+            for binary_ext in binary_exts
+        ]
+        + [["hoge.txt", False], ["foo.md", False]],
+    )
+    def test_is_binary_ext_path(value, expected):
+        assert is_binary_ext_path(value) == expected
