ca48a2d4596d3869543c4a6ef08aa3f9f2e0d9a0
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-04-04 08:34:02 +0900

    Fix a condition of pytest.mark.skipif

--- test/test_func.py
+++ test/test_func.py
@@ -42,7 +42,7 @@ class Test_detect_file_encoding(object):
 
         assert detect_file_encoding(str(p_csv)) == expected
 
-    @pytest.mark.skipif(platform.system() != "Windows", reason="platform dependent tests")
+    @pytest.mark.skipif(platform.system() == "Windows", reason="platform dependent tests")
     def test_normal_fifo(self, tmpdir):
         fifo = tmpdir.join("test_fifo")
 
