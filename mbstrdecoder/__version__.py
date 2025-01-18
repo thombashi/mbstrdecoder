48661b8f4353f6bdd380e4d67ea289a283028636
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2025-01-18 19:04:29 +0900

    Bump version to 1.1.4
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- mbstrdecoder/__version__.py
+++ mbstrdecoder/__version__.py
@@ -4,6 +4,6 @@ from typing import Final
 __author__: Final = "Tsuyoshi Hombashi"
 __copyright__: Final = f"Copyright 2016, {__author__}"
 __license__: Final = "MIT License"
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 __maintainer__: Final = __author__
 __email__: Final = "tsuyoshi.hombashi@gmail.com"
